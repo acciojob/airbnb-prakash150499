fc45c73ca7192ba183143d4e1bcbf30914c42a22
prakash150499 <121739207+prakash150499@users.noreply.github.com>
2023-09-04 11:57:48 +0530

    second

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -31,8 +31,10 @@ public class HotelManagementController {
         //incase the hotelName is null or the hotel Object is null return an empty a FAILURE
         //Incase somebody is trying to add the duplicate hotelName return FAILURE
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
-
-        return service.addHotel(hotel);
+        String s = service.addHotel(hotel);
+        if(s.equals("SUCCESS")) return "SUCCESS";
+        if(s.equals("FAILURE")) return "FAILURE";
+        return null;
     }
 
     @PostMapping("/add-user")
@@ -40,8 +42,12 @@ public class HotelManagementController {
 
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
+        Integer ans = service.addUser(user);
+        if(ans != 0){
+            return ans;
+        }
+        return null;
 
-       return service.addUser(user);
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -50,9 +56,12 @@ public class HotelManagementController {
         //Out of all the hotels we have added so far, we need to find the hotelName with most no of facilities
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
+        String s =  service.getHotelWithMostFacilities();
+        if(!s.equals(null)){
+            return s;
+        }
+        return null;
 
-
-        return service.getHotelWithMostFacilities();
     }
 
     @PostMapping("/book-a-room")
@@ -64,15 +73,22 @@ public class HotelManagementController {
         //Calculate the total amount paid by the person based on no. of rooms booked and price of the room per night.
         //If there arent enough rooms available in the hotel that we are trying to book return -1 
         //in other case return total amount paid 
-        
-        return service.bookARoom(booking);
+
+        int s = service.bookARoom(booking);
+        if(s!=0) return s;
+
+
+        return 0;
     }
     
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person 
-        return service.getBookings(aadharCard);
+        //In this function return the bookings done by a person
+        int s = service.getBookings(aadharCard);
+        if(s!=0) return s;
+        return 0;
     }
 
     @PutMapping("/update-facilities")
@@ -82,7 +98,9 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        return service.updateFacilities(newFacilities,hotelName);
+        Hotel h =  service.updateFacilities(newFacilities,hotelName);
+        if(!h.equals(null) ) return h;
+        return null;
     }
 
 }
